a3464597312c3a95d721278a326f57e76e653bf9
Esmail Khalifa <120841700+hossainmdismail@users.noreply.github.com>
2025-02-23 23:33:28 +0600

    install app

--- bootstrap/providers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    App\Providers\AppServiceProvider::class,
-    App\Providers\ConfigProvider::class,
-    App\Providers\Filament\AdminPanelProvider::class,
-];
--- resources/views/themes/default/component/loader.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="preloader">
-    <div class="clear-loading loading-effect-2">
-        <span></span>
-    </div>
-</div>
